b440d82689de0e4ee898e630f79f20002ff8b0dc
Ahmadey <madkey8@gmail.com>
2024-11-08 23:01:06 +0300

    updated the cart icon on 3 files and also updated login creditionals on shop.js and login.js

--- js/shop.js
+++ js/shop.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", async function () {
   // Sample data for gas options
   const gasOptionsData = [
-    { name: "3kg M", price: 5.5, image: "../images/2.png" },
+    { name: "3kg M", price: 5.5, image: "../images/8.png" },
     { name: "6kg M", price: 10.0, image: "../images/8.png" },
-    { name: "11kg M", price: 18.7, image: "../images/11.png" },
-    { name: "13kg M", price: 22.0, image: "../images/33.png" },
+    { name: "11kg M", price: 18.7, image: "../images/8.png" },
+    { name: "13kg M", price: 22.0, image: "../images/8.png" },
   ];
   // Load cart from local storage
   let cart = JSON.parse(localStorage.getItem("cart")) || [];
